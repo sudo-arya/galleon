73535d501a247b5270df3c0cdb70d64f58a3747b
Deepanshu Arya <whoami.arya@gmail.com>
2024-09-03 17:46:15 +0530

    updated changes according to srirag sir solved eslint errors

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import Seo from "./Seo";
-import gtlogoCircular from "../Assets/GT LOGO circular (1).png";
 
 
 
